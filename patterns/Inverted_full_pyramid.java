6e55858b9973316d413363e3ff1794efa4962c44
Abi0p <abirdattakofficial@gmail.com>
2024-02-21 00:28:34 +0530

    completed

---
+++ patterns/Inverted_full_pyramid.java
@@ -0,0 +1,25 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Inverted_full_pyramid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("enter no. of rows");
+        n=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=1;j< i;j++){
+                System.out.print(" ");
+            
+            
+            }
+            for (int k=n;k>=i;k--){
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+    }
+    
+    
+}
