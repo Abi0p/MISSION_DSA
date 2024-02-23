2c2f7c9bfe10db3bebee798cc79dc378d48524e9
Abi0p <abirdattakofficial@gmail.com>
2024-02-23 23:58:44 +0530

    print

---
+++ string_in_python/basic_operations.py
@@ -0,0 +1,5 @@
+string1 = ("starting new coding experience with python")
+print(string1) # printing the given string
+print (string1.find('s')) #printing the index of the particular character of a string 
+print (string1[0:9]) #slicing of the string   
+
